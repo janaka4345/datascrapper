a2fb91caf0e439ed595f0a3a8e5dca79d9c345c8
Janaka dev <janakchamantha12@gmail.com>
2024-08-09 19:48:30 +0530

    feat:scrape json api endpoint

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -1,10 +1,44 @@
 import scrapy
+import json
 
 
 class BookspiderSpider(scrapy.Spider):
     name = "bookspider"
-    allowed_domains = ["books.toscrape.com"]
-    start_urls = ["https://books.toscrape.com/"]
+    url = "https://jsonplaceholder.typicode.com/todos"
+    # allowed_domains = ["books.toscrape.com"]
+    # start_urls = ["https://books.toscrape.com/"]
 
-    def parse(self, response):
-        pass
+    # def parse(self, response):
+    #     books = response.css("article.product_pod")
+    #     print(len(books))
+    #     for book in books:
+    #         yield {
+    #             "name": book.css("h3 a::text").get(),
+    #             "price": book.css("p.price_color::text").get(),
+    #         }
+
+    #     next_page = response.css("li.next a").attrib["href"]
+    #     if next_page is not None:
+    #         print(f"{self.start_urls[0]}{next_page}")
+    #         # if "catalogue/" in next_page:
+    #         next_page_url = f"{self.start_urls[0]}{next_page}"
+    #         # else:
+    #         # next_page_url = f"{self.start_urls[0]}catalogue/{next_page}"
+
+    #         yield response.follow(next_page_url, callback=self.parse)
+    def start_requests(self):
+        print("***************************************************")
+        yield scrapy.Request(url=self.url, callback=self.discover)
+
+    def discover(self, response):
+        print(f"*******{type(response)}")
+        python_dict = json.loads(response.text)
+        print(f"*******{type(python_dict)}")
+        print(python_dict[0]["title"])
+        for item in python_dict:
+            yield {
+                "userId": item["userId"],
+                "id": item["id"],
+                "title": item["title"],
+                "completed": item["completed"],
+            }
