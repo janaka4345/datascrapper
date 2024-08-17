04687d65976aa0b0921aa14b021bde6249a60386
Janaka dev <janakchamantha12@gmail.com>
2024-08-17 20:13:14 +0530

    other:items added for items.py

--- bookscraper/bookscraper/items.py
+++ bookscraper/bookscraper/items.py
@@ -10,3 +10,19 @@ class BookscraperItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+
+class BookscraperTodoItem(scrapy.Item):
+    userId = scrapy.Field()
+    id = scrapy.Field()
+    title = scrapy.Field()
+    completed = scrapy.Field()
+
+
+class BookscraperUserItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    id = scrapy.Field()
+    name = scrapy.Field()
+    username = scrapy.Field()
+    email = scrapy.Field()
